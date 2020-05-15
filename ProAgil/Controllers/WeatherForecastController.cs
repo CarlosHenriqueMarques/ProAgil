865b27a73a7f89b868c3081b0ecb3f2cfb199fe5
Carlos Maia <carlos.marques89@gmail.com>
2020-05-15 15:47:09 -0400

    Primeiro commit

---
+++ ProAgil/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProAgil.Model;
+
+namespace ProAgil.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        { /*
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();*/
+            return new Evento[]
+            {
+                new Evento()
+                {
+                    EventoId =1,
+                    Tema = "Angular e .NET core",
+                    Local = "Toronto",
+                    Lote = "5",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento()
+                {
+                    EventoId =2,
+                    Tema = "Angular",
+                    Local = "Waterloo",
+                    Lote = "6",
+                    QtdPessoas = 800,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                }
+            };
+        }
+    }
+}
